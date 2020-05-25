d6115e705ad44a4fce4ba352c1d390e5122b8dae
Sanjana Warambhey <wsanjana19@gmail.com>
2020-05-25 22:50:14 +0530

    Initial Commit

---
+++ RatingSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingSystem
+//
+//  Created by Sanjana Warambhey on 25/05/20.
+//  Copyright © 2020 Sanjana Warambhey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
